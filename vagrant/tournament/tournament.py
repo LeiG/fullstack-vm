de10b459cb5191012f60e6ba11bddeb6de110da3
Lei Gong <leig.inbox@gmail.com>
2017-03-25 11:46:39 -0700

    Add tournament implementation

--- vagrant/tournament/tournament.py
+++ vagrant/tournament/tournament.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# 
+#
 # tournament.py -- implementation of a Swiss-system tournament
 #
 
@@ -13,32 +13,55 @@ def connect():
 
 def deleteMatches():
     """Remove all the match records from the database."""
+    conn = connect()
+    cur = conn.cursor()
+    cur.execute("DELETE FROM matches;")
+    conn.commit()
+    conn.close()
 
 
 def deletePlayers():
     """Remove all the player records from the database."""
+    conn = connect()
+    cur = conn.cursor()
+    cur.execute("DELETE FROM players;")
+    conn.commit()
+    conn.close()
 
 
 def countPlayers():
     """Returns the number of players currently registered."""
+    conn = connect()
+    cur = conn.cursor()
+    cur.execute("SELECT COUNT(*) FROM players;")
+    numPlayers = cur.fetchone()[0]
+    conn.commit()
+    conn.close()
+
+    return numPlayers
 
 
 def registerPlayer(name):
     """Adds a player to the tournament database.
-  
+
     The database assigns a unique serial id number for the player.  (This
     should be handled by your SQL database schema, not in your Python code.)
-  
+
     Args:
       name: the player's full name (need not be unique).
     """
+    conn = connect()
+    cur = conn.cursor()
+    cur.execute("INSERT INTO players (id, name) VALUES (DEFAULT, %s)", (name,))
+    conn.commit()
+    conn.close()
 
 
 def playerStandings():
     """Returns a list of the players and their win records, sorted by wins.
 
-    The first entry in the list should be the player in first place, or a player
-    tied for first place if there is currently a tie.
+    The first entry in the list should be the player in first place, or a
+    player tied for first place if there is currently a tie.
 
     Returns:
       A list of tuples, each of which contains (id, name, wins, matches):
@@ -47,6 +70,14 @@ def playerStandings():
         wins: the number of matches the player has won
         matches: the number of matches the player has played
     """
+    conn = connect()
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM standings ORDER BY wins DESC;")
+    standings = cur.fetchall()
+    conn.commit()
+    conn.close()
+
+    return standings
 
 
 def reportMatch(winner, loser):
@@ -56,16 +87,24 @@ def reportMatch(winner, loser):
       winner:  the id number of the player who won
       loser:  the id number of the player who lost
     """
- 
- 
+    conn = connect()
+    cur = conn.cursor()
+    cur.execute(
+        "INSERT INTO matches (id, winner_id, loser_id)"
+        "VALUES (DEFAULT, %d, %d)" % (winner, loser)
+    )
+    conn.commit()
+    conn.close()
+
+
 def swissPairings():
     """Returns a list of pairs of players for the next round of a match.
-  
+
     Assuming that there are an even number of players registered, each player
     appears exactly once in the pairings.  Each player is paired with another
     player with an equal or nearly-equal win record, that is, a player adjacent
     to him or her in the standings.
-  
+
     Returns:
       A list of tuples, each of which contains (id1, name1, id2, name2)
         id1: the first player's unique id
@@ -73,5 +112,30 @@ def swissPairings():
         id2: the second player's unique id
         name2: the second player's name
     """
+    player_standings = playerStandings()
 
+    conn = connect()
+    cur = conn.cursor()
+    cur.execute("SELECT winner_id, loser_id FROM matches;")
+    matches = set(cur.fetchall())
+    conn.commit()
+    conn.close()
 
+    pairings = []
+    while len(player_standings) > 0:
+        candidate_a = player_standings.pop(0)
+        for candidate_b in player_standings:
+            if (candidate_a, candidate_b) not in matches\
+               and (candidate_b, candidate_a) not in matches:
+                pairings.append(
+                    (
+                        candidate_a[0],
+                        candidate_a[1],
+                        candidate_b[0],
+                        candidate_b[1],
+                    )
+                )
+                player_standings.remove(candidate_b)
+                break
+
+    return pairings
--- vagrant/tournament/tournament.sql
+++ vagrant/tournament/tournament.sql
@@ -6,4 +6,48 @@
 -- You can write comments in this file by starting them with two dashes, like
 -- these lines here.
 
+DROP DATABASE IF EXISTS tournament;
 
+CREATE DATABASE tournament;
+\c tournament;
+
+CREATE TABLE players(
+  id    SERIAL  UNIQUE,
+  name  TEXT    NOT NULL
+);
+
+CREATE TABLE matches(
+  id         SERIAL  UNIQUE,
+  winner_id  INT     NOT NULL,
+  loser_id   INT     NOT NULL
+);
+
+CREATE VIEW win AS
+  SELECT
+    players.id AS id,
+    MIN(players.name) AS name,
+    SUM(CASE WHEN matches.winner_id IS NOT NULL THEN 1 ELSE 0 END) AS wins
+  FROM matches
+  FULL OUTER JOIN players
+  ON players.id = matches.winner_id
+  GROUP BY players.id;
+
+CREATE VIEW loss AS
+  SELECT
+    players.id AS id,
+    MIN(players.name) AS name,
+    SUM(CASE WHEN matches.loser_id IS NOT NULL THEN 1 ELSE 0 END) AS losses
+  FROM matches
+  FULL OUTER JOIN players
+  ON players.id = matches.loser_id
+  GROUP BY players.id;
+
+CREATE VIEW standings AS
+  SELECT
+    win.id AS id,
+    win.name AS name,
+    win.wins AS wins,
+    win.wins + loss.losses AS matches
+  FROM win
+  JOIN loss
+  ON win.id = loss.id
