906f3aeaf2f6b8e884a5b3f590aec429b6ab4124
Lei Gong <leig.inbox@gmail.com>
2017-04-04 20:47:36 -0700

    Add templates and assets

---
+++ vagrant/catalog/fake_data.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+
+topic = {'id': '1', 'name': 'programming', 'created_date': '2017-01-01'}
+
+topics = [
+    {'id': '1', 'name': 'programming', 'created_date': '2017-01-01'},
+    {'id': '2', 'name': 'reading', 'created_date': '2017-02-01'},
+    {'id': '3', 'name': 'workout', 'created_date': '2017-03-01'},
+]
+
+card = {
+    'id': '1',
+    'name': 'scala',
+    'content': 'scala is a functional programming language',
+    'created_at': '2017-04-01',
+    'updated_at': '2017-04-02',
+    'topic_id': '1',
+    'user_id': '1',
+}
+
+cards = [
+    {
+        'id': '1',
+        'name': 'scala',
+        'content': 'scala is a functional programming language',
+        'created_at': '2017-04-01',
+        'updated_at': '2017-04-02',
+        'topic_id': '1',
+        'user_id': '1',
+    },
+    {
+        'id': '2',
+        'name': 'python',
+        'content': 'python is a programming language',
+        'created_at': '2017-04-02',
+        'updated_at': '2017-04-02',
+        'topic_id': '1',
+        'user_id': '1',
+    },
+    {
+        'id': '3',
+        'name': 'cookbook',
+        'content': 'cookbook is great for beginners',
+        'created_at': '2017-04-03',
+        'updated_at': '2017-04-04',
+        'topic_id': '2',
+        'user_id': '1',
+    },
+]
+
+user = {'id': '1', 'username': 'lei', 'email': 'lei@commitlogs.com'}
--- vagrant/catalog/til.py
+++ vagrant/catalog/til.py
@@ -2,10 +2,12 @@
 # -*- coding: utf-8 -*-
 
 from flask import Flask
+from flask import render_template
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 from database_setup import Base, User, Topic, Card
+import fake_data
 
 app = Flask(__name__)
 
@@ -17,49 +19,78 @@ db_session = sessionmaker(bind=engine)
 session = db_session()
 
 
+@app.route('/login/', methods=['GET', 'POST'])
+def login():
+    return render_template('login.html')
+
+
+@app.route('/signup/', methods=['GET', 'POST'])
+def signup():
+    return render_template('signup.html')
+
+
+@app.route('/logout/', methods=['GET', 'POST'])
+def logout():
+    return "You logged out..."
+
+
 @app.route('/')
-@app.route('/topics')
-def Topics():
-    all_topics = session.query(Topic).all()
-    latest_cards =\
-        session.query(Card).order_by(Card.updated_date.desc()).limit(10).all()
+@app.route('/topics/')
+def showTopics():
+    # all_topics = session.query(Topic).all()
+    # latest_cards =\
+    #     session.query(Card).order_by(Card.updated_date.desc()).limit(10).all()
 
-    return "This is the /topics page"
-
-
-@app.route('/topics/new')
-def NewTopic():
-    return "This is the /topics/new page"
-
-
-@app.route('/topics/<int:topic_id>/edit')
-def EditTopic(topic_id):
-    return "This is the /topics/%s/edit page" % str(topic_id)
+    return render_template(
+        'topics.html',
+        all_topics=fake_data.topics,
+        latest_cards=fake_data.cards,
+    )
 
 
-@app.route('/topics/<int:topic_id>/delete')
-def DeleteTopic(topic_id):
-    return "This is the /topics/%s/delete page" % str(topic_id)
+@app.route('/topics/new/', methods=['GET', 'POST'])
+def newTopic():
+    return render_template('new-topic.html')
 
 
-@app.route('/topics/<int:topic_id>/cards')
-def Cards(topic_id):
-    return "This is the /topics/%s/cards page" % str(topic_id)
+@app.route('/topics/<int:topic_id>/edit/', methods=['GET', 'POST'])
+def editTopic(topic_id):
+    return render_template('edit-topic.html')
 
 
-@app.route('/topics/<int:topic_id>/cards/new')
-def NewCard(topic_id):
-    return "This is the /topics/%s/cards/new page" % str(topic_id)
+@app.route('/topics/<int:topic_id>/delete/', methods=['GET', 'POST'])
+def deleteTopic(topic_id):
+    return render_template(
+        'topics.html',
+        all_topics=fake_data.topics,
+        latest_cards=fake_data.cards,
+    )
 
 
-@app.route('/topics/<int:topic_id>/cards/<int:card_id>/edit')
-def EditCard(topic_id, card_id):
-    return "This is the /topics/%s/cards/%s/edit page" % (str(topic_id), str(card_id))
+@app.route('/topics/<int:topic_id>/')
+@app.route('/topics/<int:topic_id>/cards/')
+def showCards(topic_id):
+    return render_template(
+        'cards.html',
+        all_topics=fake_data.topics,
+        all_cards=fake_data.cards,
+    )
 
 
-@app.route('/topics/<int:topic_id>/cards/<int:card_id>/delete')
-def DeleteCard(topic_id, card_id):
-    return "This is the /topics/%s/cards/%s/delete page" % (str(topic_id), str(card_id))
+@app.route('/topics/<int:topic_id>/cards/new/')
+def newCard(topic_id):
+    return render_template('new-card.html')
+
+
+@app.route('/topics/<int:topic_id>/cards/<int:card_id>/edit/')
+def editCard(topic_id, card_id):
+    return render_template('edit-card.html')
+
+
+@app.route('/topics/<int:topic_id>/cards/<int:card_id>/delete/')
+def deleteCard(topic_id, card_id):
+    return "This is the /topics/%s/cards/%s/delete page"\
+        % (str(topic_id), str(card_id))
 
 
 if __name__ == "__main__":
